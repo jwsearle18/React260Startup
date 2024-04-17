4f4b35087e7f6466c92f59d68a7b9bb91a8bb7bc
jwsearle18 <144712016+jwsearle18@users.noreply.github.com>
2024-04-17 14:32:56 -0600

    vrCompanies fully functional component

--- src/vrCompanies/vrCompanies.jsx
+++ src/vrCompanies/vrCompanies.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 export function Vrcompanies() {
     return (
         <main className='container-fluid bg-secondary text-center'>
-            <div>VRcompanies displayed here</div>
+            <h1>Find a Vacation Rental!</h1>
+            <a href="https://www.airbnb.com/">
+            <img src="/images/airbnb.png" alt="Airbnb logo"/>
+          </a>
+          <a href="https://www.vrbo.com/">
+            <img src="/images/vrbo.png" alt="Vrbo logo"/>
+          </a>
+          <a href="shttps://www.hometogo.com/">
+            <img src="/images/hometogo.jpeg" alt="HomeToGo logo"/>
+          </a>
         </main>
     )
 }
